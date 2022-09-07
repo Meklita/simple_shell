50e0a673e951afbcf20a3b9e40e16834451931fb
Meklita <mektnz@gmail.com>
2022-09-07 13:42:45 -0700

    final

--- shell.h
+++ shell.h
@@ -9,7 +9,23 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <errno.h>
-
+int lsh_ctrld(char **args);
+int lsh_cd(char **args);
+int lsh_help(char **args);
+extern char **environ;
+int lsh_exit(char **args);
+int _strcmp(char *s1, char *s2);
+size_t _strncmp(char *s1, char *s2, size_t n);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+int _putchar(char c);
+char *_get_path(char **env);
+int _values_path(char **arg, char **env);
 char *_getline_command(void);
-int main(int ac, char **av, char **env);
 void _getenv(char **env);
+char **tokenize(char *lineptr);
+void _exit_command(char **args, char *lineptr, int _exit);
+int _fork_fun(char **arg, char **av, char **env, char *lineptr, int np, int c);
+
+#endif /* SHELL_H */
